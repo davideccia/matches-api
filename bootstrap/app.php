23e51ffd5eed6e38e5354b2d84c2eda2d4374b29
davideccia <davideccia@gmail.com>
2026-06-16 17:40:30 +0200

    fix(app): frontend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Middleware\SetLocale;
+use Illuminate\Auth\Middleware\Authorize;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -27,6 +29,14 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->api(prepend: [
             SetLocale::class,
+        ]);
+
+        $middleware->alias([
+            'ability' => CheckForAnyAbility::class,
+        ]);
+
+        $middleware->priority([
+            Authorize::class,
         ]);
 
     })

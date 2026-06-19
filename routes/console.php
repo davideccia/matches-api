e44309d4631a6ab26e35ffb870fa09c7ad69b2db
davideccia <davideccia@gmail.com>
2026-06-19 19:44:49 +0200

    feat: backup

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(CleanupTemporaryUploadsCommand::class)->everySixHours()->withoutOverlapping();
 Schedule::command('horizon:snapshot')->everyFiveMinutes();
+
+Schedule::command('backup:clean')->daily()->at('01:00')->withoutOverlapping();
+Schedule::command('backup:run')->daily()->at('01:30')->withoutOverlapping();
+Schedule::command('backup:monitor')->daily()->at('02:00');

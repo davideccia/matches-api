d1fe5f4ea3bd2097aab30439698d736350e19ed8
davideccia <davideccia@gmail.com>
2026-09-22 20:54:18 +0200

    feat(app): reverb on Pulse

--- config/pulse.php
+++ config/pulse.php
@@ -183,6 +183,10 @@ return [
             ],
         ],
 
+        \Laravel\Reverb\Pulse\Recorders\ReverbConnections::class => [
+            'sample_rate' => env('PULSE_REVERB_CONNECTIONS_SAMPLE_RATE', 1),
+        ],
+
         Recorders\Servers::class => [
             'server_name' => env('PULSE_SERVER_NAME', gethostname()),
             'directories' => explode(':', env('PULSE_SERVER_DIRECTORIES', '/')),
--- resources/views/vendor/pulse/dashboard.blade.php
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -1,19 +1,33 @@
+@php
+    $sectionHeading = fn (string $title) => '<h3 class="col-span-12 mt-2 first:mt-0 text-lg font-bold uppercase tracking-wider text-gray-500 dark:text-gray-400 border-b border-gray-200 dark:border-gray-800 pb-3">'.e($title).'</h3>';
+@endphp
+
 <x-pulse>
-    <livewire:pulse.servers cols="full" />
+    {!! $sectionHeading('Infrastructure') !!}
 
-    <livewire:pulse.usage cols="4" rows="2" />
+    <livewire:pulse.servers cols="full"/>
 
-    <livewire:pulse.queues cols="4" />
+    <livewire:pulse.usage cols="12" rows="2"/>
 
-    <livewire:pulse.cache cols="4" />
+    {!! $sectionHeading('Realtime') !!}
 
-    <livewire:pulse.slow-queries cols="8" />
+    <livewire:reverb.connections cols="full"/>
 
-    <livewire:pulse.exceptions cols="6" />
+    {!! $sectionHeading('Queues & Cache') !!}
 
-    <livewire:pulse.slow-requests cols="6" />
+    <livewire:pulse.queues cols="4"/>
 
-    <livewire:pulse.slow-jobs cols="6" />
+    <livewire:pulse.cache cols="4"/>
 
-    <livewire:pulse.slow-outgoing-requests cols="6" />
+    <livewire:pulse.slow-outgoing-requests cols="4"/>
+
+    {!! $sectionHeading('Performance & Errors') !!}
+
+    <livewire:pulse.slow-queries cols="12"/>
+
+    <livewire:pulse.exceptions cols="6"/>
+
+    <livewire:pulse.slow-requests cols="6"/>
+
+    <livewire:pulse.slow-jobs cols="12"/>
 </x-pulse>
